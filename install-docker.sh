6705e1fa11ef7a0d34e1fde62bbdcfdbdcf224af
Sathyaraj06 <46516308+Sathyaraj06@users.noreply.github.com>
2023-05-24 20:13:16 +0000

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -741,3 +741,7 @@ do_install() {
 # wrapped up in a function so that we have some protection against only getting
 # half the file during "curl | sh"
 do_install
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
+sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
