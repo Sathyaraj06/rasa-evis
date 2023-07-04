00b633827e4ca51fad5c666c50c2218ef77197fe
Sathyaraj06 <sathyarajktsamy@gmail.com>
2023-07-04 19:39:50 +0000

    changes

---
+++ docker/actions-bg.sh
@@ -0,0 +1,9 @@
+dpkg -S /usr/bin/nohup
+
+env > /tmp/cron.job
+
+which vmpstat mpstat iostat
+
+nohup sudo rasa run -d --enable-api --log-file rasa.log --debug --cors "*" 
+
+nohup sudo rasa run actions &
