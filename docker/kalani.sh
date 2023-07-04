7699b5f8d0d994d6bba752c2dc9b1d50da37e849
Sathyaraj06 <sathyarajktsamy@gmail.com>
2023-07-04 18:36:33 +0000

    fix

--- docker/kalani.sh
+++ docker/kalani.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 org=${1}
 
-if [ "$org" == "kalani" ]; then
-        rasa train --data data/base/nlu.yml data/hexaware/nlu.yml data/base/rules.yml data/hexaware/rules.yml data/base/stories.yml data/hexaware/stories.yml -d data/base/domain.yml data/hexaware/domain.yml --config data/hexaware/config.yml --out models --fixed-model-name model
-fi
+echo $org;
+
+# if [ "$org" == "kalani" ]; then
+#         rasa train --data data/base/nlu.yml data/hexaware/nlu.yml data/base/rules.yml data/hexaware/rules.yml data/base/stories.yml data/hexaware/stories.yml -d data/base/domain.yml data/hexaware/domain.yml --config data/hexaware/config.yml --out models --fixed-model-name model
+# fi
