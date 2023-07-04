d43a0b298cdcb06b48398b6c9794d87872f64e0f
Sathyaraj06 <sathyarajktsamy@gmail.com>
2023-07-04 18:43:55 +0000

    af

--- docker/kalani.sh
+++ docker/kalani.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-org=${1}
+org="sathy"
 
 if [ "$org" == "kalani" ]; then
         rasa train --data data/base/nlu.yml data/hexaware/nlu.yml data/base/rules.yml data/hexaware/rules.yml data/base/stories.yml data/hexaware/stories.yml -d data/base/domain.yml data/hexaware/domain.yml --config data/hexaware/config.yml --out models --fixed-model-name model
