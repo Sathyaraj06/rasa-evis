a116d67d17138cd5c8e417bad63d35847f32454c
Sathyaraj06 <sathyarajktsamy@gmail.com>
2023-07-04 19:43:16 +0000

    fdg

--- docker/actions-bg.sh
+++ docker/actions-bg.sh
@@ -4,6 +4,6 @@ env > /tmp/cron.job
 
 which vmpstat mpstat iostat
 
-nohup sudo rasa run -d --enable-api --log-file rasa.log --debug --cors "*" 
+# nohup sudo rasa run -d --enable-api --log-file rasa.log --debug --cors "*" 
 
 nohup sudo rasa run actions &
