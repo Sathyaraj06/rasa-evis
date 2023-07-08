c92e969dbd0575905d923140c3571bdda8cfcadb
Sathyaraj06 <sathyarajktsamy@gmail.com>
2023-07-08 16:58:55 +0000

    changes

--- install_ssh.sh
+++ install_ssh.sh
@@ -1,9 +1,12 @@
 #install ssh server
-
-docker run --name webssh2 -d -p 8001:2222 billchurch/webssh2
+sudo docker stop webssh2
+sudo docker rm webssh2
+sudo docker run --name webssh2 -d -p 8001:2222 billchurch/webssh2
 
 #install portainer.io
 
 sudo docker volume create portainer_data
 
+sudo docker stop portainer
+sudo docker rm portainer
 sudo docker run -d -p 8000:8000 -p 8002:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
