59a969824c9f9b827de624f1ff130c89e70b8bc1
Sathyaraj06 <sathyarajktsamy@gmail.com>
2023-07-08 17:59:45 +0000

    changes

---
+++ deploy_evis.sh
@@ -0,0 +1,12 @@
+echo "started---------------"
+
+git reset --hard
+
+git pull
+
+sudo chmod 777 deploy_evis.sh
+
+sudo docker-compose up --build --force-recreate -d evis
+
+echo "ended---------------"
+
