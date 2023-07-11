c5181020575b82b981c99b6a73c7aa33e3a98e4d
Sathyaraj06 <sathyarajktsamy@gmail.com>
2023-07-11 03:25:56 +0000

    wer

---
+++ deploy_evisalone.sh
@@ -0,0 +1,12 @@
+echo "started---------------"
+
+git reset --hard
+
+git pull
+
+sudo chmod 777 deploy_evisalone.sh
+
+sudo docker-compose up --build --force-recreate -d evisalone
+
+echo "ended---------------"
+
