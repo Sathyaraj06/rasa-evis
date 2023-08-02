e031b557ec865a2883dd86ee612018b6ff82227e
sathyaraj06 <sathyarajktsamy@gmail.com>
2023-08-02 13:34:27 +0000

    changes

--- support/mysql_backup/mysql-backup.sh
+++ support/mysql_backup/mysql-backup.sh
@@ -18,12 +18,13 @@ BACKUP_RETAIN_DAYS=30   ## Number of days to keep local backup copy
 sudo mkdir -p ${DB_BACKUP_PATH}/${TODAY}
 echo "Backup started for database - ${DATABASE_NAME}"
 
-
 sudo mysqldump -h ${MYSQL_HOST} \
 		  -P ${MYSQL_PORT} \
 		  -u ${MYSQL_USER} \
 		  -p${MYSQL_PASSWORD} \
-		  ${DATABASE_NAME} | gzip > ${DB_BACKUP_PATH}/${TODAY}/${DATABASE_NAME}-${TODAY}.sql.gz
+		  ${DATABASE_NAME} > ${DB_BACKUP_PATH}/${TODAY}/${DATABASE_NAME}-${TODAY}.sql
+
+      		  # ${DATABASE_NAME} | gzip > ${DB_BACKUP_PATH}/${TODAY}/${DATABASE_NAME}-${TODAY}.sql.gz
 
 if [ $? -eq 0 ]; then
   echo "Database backup successfully completed"
