9288bd63097581fd14f6355d6a943e34de8a00a9
sathyaraj06 <sathyarajktsamy@gmail.com>
2023-08-06 14:34:25 +0000

    rasa changes

--- deploy_nginxproxy.sh
+++
@@ -1,6 +0,0 @@
-echo "started---------------"
-
-sudo docker-compose up --build --force-recreate -d nginxproxy
-
-echo "ended---------------"
-
--- support/install-dns.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get install bind9
-
-cat /etc/bind/named.conf
-
-#check if correct
-named-checkconf /path/to/named.conf
-
-sudo systemctl restart bind9
-
